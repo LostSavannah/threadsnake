e89fa4c53881940a4332b33afae64679500a8ca9
codeRookieErick <erickfernandomoraramirez@gmail.com>
2022-08-16 23:43:56 -0400

    Ommiting package publish. Adding more test cases

--- src/threadsnake/testing.py
+++ src/threadsnake/testing.py
@@ -5,16 +5,25 @@ TestFunction = Callable[[Any], bool]
 testActions = []
 
 class TestAction:
+    printParameters = False
     def __init__(self, f:Callable, params:Dict[str, Any], expects:bool):
+        def action():
+            parameters = {a:params[a]() for a in params}
+            if TestAction.printParameters:
+                print('Parameters:')
+                for p in parameters:
+                    print(f'\t{p} = {parameters[p]}')
+            return f.__call__(**{a:params[a]() for a in params}) == expects
+
         self.name = f.__name__
-        self.action = lambda: f.__call__(**params) == expects
+        self.action = action
 
     def __call__(self) -> bool:
         return self.action()
 
 def test(params:Dict[str, Any], expects:bool) -> TestFunction:
     def inner(f:TestFunction) -> TestFunction:
-        testActions.append(TestAction(f, params, expects))
+        testActions.insert(0, TestAction(f, params, expects))
         return f
     return inner
 
@@ -22,14 +31,16 @@ def run_test():
     tests = len(testActions)
     success = 0
     while len(testActions) > 0:
-        t = testActions.pop()
-        badge_async(t.name, LogColors.DARK_BACKGROUND_BLACK, 'testing', LogColors.DARK_BACKGROUND_YELLOW, False)
-        if t():
-            badge_async(t.name, LogColors.DARK_BACKGROUND_BLACK, 'passed', LogColors.DARK_BACKGROUND_GREEN)
-            success += 1
-        else:
-            badge_async(t.name, LogColors.DARK_BACKGROUND_BLACK, 'failed', LogColors.DARK_BACKGROUND_RED)
-
+        try:
+            t = testActions.pop()
+            badge_async(t.name, LogColors.DARK_BACKGROUND_BLACK, 'testing :|', LogColors.DARK_BACKGROUND_YELLOW, False)
+            if t():
+                badge_async(t.name, LogColors.DARK_BACKGROUND_BLACK, '+passed :)', LogColors.DARK_BACKGROUND_GREEN)
+                success += 1
+            else:
+                badge_async(t.name, LogColors.DARK_BACKGROUND_BLACK, '-failed :(', LogColors.DARK_BACKGROUND_RED)
+        except Exception as e:
+            print(e)
     badge_async(str(tests), LogColors.DARK_BACKGROUND_BLACK, 'test cases :', LogColors.DARK_BACKGROUND_BLUE, padding=False)
     badge_async(str(success), LogColors.DARK_BACKGROUND_BLACK, 'test passed:', LogColors.DARK_BACKGROUND_GREEN, padding=False)
     badge_async(str(tests - success), LogColors.DARK_BACKGROUND_BLACK, 'test failed:', LogColors.DARK_BACKGROUND_RED, padding=False)
--- test/test-server.py
+++ test/test-server.py
@@ -3,10 +3,11 @@ from threadsnake.testing import test, run_test
 from typing import Callable, Any
 import requests
 import time
-
+import random
+import hashlib
 
 #set_log_config(LogLevel.ALL)
-badge_async('threadsnake 0.0.19', title='version')
+badge_async('threadsnake 0.0.23', title='version')
 
 
 
@@ -34,15 +35,80 @@ app.configure(serve_static_markdown('markdown'))
 #Configuring routers
 app.use_router(routes_to('routers/users'), '/users')
 
-@app.get('/test-api/base-endpoint/{param}')
-def test_api_base_endpoint(app:Application, req:HttpRequest, res:HttpResponse):
+def get_word(length:int = None, vowelChance:float = None):
+    length = length or random.randint(2, 10)
+    vowelChance = vowelChance or 0.5 + (random.random() / 3)
+    vowels = [i for i in 'aeiou']
+    consonants = [chr(i) for i in range(ord('a'), ord('z')+1) if chr(i) not in vowels]
+    res = []
+    for i in range(length):
+        if random.random() > vowelChance:
+            res.append(random.choice(consonants))
+        else:
+            res.append(random.choice(vowels))
+    return ''.join(res)
+
+def get_file():
+    return 'this is a file'.encode('latin1')
+
+def get_hash(data):
+    return hashlib.md5(data).hexdigest()
+
+@app.get('/test-api/querypath/{param}')
+def test_querypath(app:Application, req:HttpRequest, res:HttpResponse):
     res.end(req.params['param'])
 
-@test({"parameter":"Pelusa"}, True)
-def when_param_sent_via_querystring_on_get_it_get_returned_as_response(parameter:str = ''):
-    url = f'http://localhost:{port}/test-api/base-endpoint/{parameter}'
+@app.get('/test-api/querystring')
+@requires_parameters(['param'])
+def test_querystring(app:Application, req:HttpRequest, res:HttpResponse):
+    res.end(req.params['param'])
+
+@app.get('/test-api/pass')
+@requires_parameters(['param'])
+def test_querystring(app:Application, req:HttpRequest, res:HttpResponse):
+    res.end(req.params['param'])
+
+@app.post('/test-api/multipart-form-data')
+@requires_parameters(['param'])
+def test_multipart_form_data(app:Application, req:HttpRequest, res:HttpResponse):
+    res.end(req.params['param'])
+
+@app.post('/test-api/multipart-form-data/file')
+def test_multipart_form_data_file(app:Application, req:HttpRequest, res:HttpResponse):
+    if len(req.files) > 0:
+        with open([i for i in req.files.values()][0]['tempFilePath'], 'rb') as file:
+            res.end(get_hash(file.read()))
+    else:
+        res.end('')
+
+
+#TEST
+@test({"parameter":get_word}, True)
+def when_param_sent_via_querypath_on_get_it_is_returned_as_response(parameter:str = ''):
+    url = f'http://localhost:{port}/test-api/querypath/{parameter}'
     return requests.get(url).text == parameter
     
+@test({"parameter":get_word}, True)
+def when_param_sent_via_querystring_on_get_it_is_returned_as_response(parameter:str = ''):
+    url = f'http://localhost:{port}/test-api/querystring?param={parameter}'
+    return requests.get(url).text == parameter
+
+@test({"parameter":get_word}, True)
+def when_param_sent_via_pass_on_get_it_is_returned_as_response(parameter:str = ''):
+    url = f'http://localhost:{port}/test-api/param:{parameter}/pass'
+    return requests.get(url).text == parameter
+
+@test({"multipartParameter": get_word}, True)
+def when_param_sent_via_multipart_on_post_it_is_ok(multipartParameter:str = ''):
+    url = f'http://localhost:{port}/test-api/multipart-form-data'
+    return requests.post(url, files={"param": (None, multipartParameter)}).text == multipartParameter
+
+@test({"fileContent": get_file}, True)
+def when_file_sent_via_multipart_on_post_it_is_ok(fileContent:str = ''):
+    url = f'http://localhost:{port}/test-api/multipart-form-data/file'
+    contentHash = get_hash(fileContent)
+    return contentHash == requests.post(url, files={"file": ("testfile.txt", fileContent)}).text
+
 try:
     app.start()
     time.sleep(1)
