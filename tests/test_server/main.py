7b708554afae2cf8d3b4aa7e00eb1b0301193560
codeRookieErick <erickfernandomoraramirez@gmail.com>
2023-04-09 10:31:55 -0400

    Adding test server

--- tests/test_server/main.py
+++ tests/test_server/main.py
@@ -1,2 +1,15 @@
+import asyncio
+from threadsnake.turbo import Application
+from threadsnake.http.tools.routing import routes_to_folder
+
 import os
-os.system('pytest')
+
+app = Application(9090)
+routes_to_folder(app, 'routers')
+
+async def run_test_server_and_test():
+    task = asyncio.create_task(app.run())
+    os.system('pytest')
+    task.cancel()
+
+asyncio.run(run_test_server_and_test())
